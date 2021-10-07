8e8e74f49c7df7e60c5a0fb3d57ce208d9762bf4
Beatriz <biiadiiego@gmail.com>
2021-10-07 12:27:02 -0300

    Implementação do get e post de temas

---
+++ blogDaBia/src/app/service/tema.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TemaService {
+
+  constructor(
+
+    private http: HttpClient
+  ) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  getAllTema(): Observable<Tema[]>{
+    return this.http.get<Tema[]>('http://localhost:8080/temas', this.token)
+  }
+
+  postTema(tema: Tema): Observable<Tema>{
+    return this.http.post<Tema>('http://localhost:8080/temas/novotema', tema, this.token)
+
+  }
+}
