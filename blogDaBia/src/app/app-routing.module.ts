c99d8033812c8e387d204fc35d30b50ac324eb05
Beatriz <biiadiiego@gmail.com>
2021-10-07 10:37:13 -0300

    Componente tema sem consumo

--- blogDaBia/src/app/app-routing.module.ts
+++ blogDaBia/src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { TemaComponent } from './tema/tema.component';
 
 const routes: Routes = [
 
   {path:'', redirectTo:'entrar', pathMatch:'full'},
   {path:'entrar', component:EntrarComponent}, 
   {path:'cadastrar', component:CadastrarComponent},
-  {path: 'inicio', component:InicioComponent}
+  {path: 'inicio', component:InicioComponent},
+  {path: 'tema', component:TemaComponent}
+
 
 ];
 
